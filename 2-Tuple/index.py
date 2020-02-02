6c965401eb579781223c7d30b42ae7e72de29d3c
unknown <duzguntutar@gmail.com>
2020-02-02 23:23:07 +0300

    ..

---
+++ 2-Tuple/index.py
@@ -0,0 +1,15 @@
+
+#Tuplelar değiştirlemez.
+
+myTuple = (1,2,3,4,6,4,8,4,4,4)
+
+
+print("-"*50)
+print(myTuple)
+
+print("-"*50)
+print(myTuple.index(2))
+
+print("-"*50)
+print(myTuple.count(4))
+
---
+++ 3-Dictionary/index.py
@@ -0,0 +1,24 @@
+
+# 
+
+a = {"ali":25,"veli":33,"coşkun":45}
+
+print("-"*50)
+print(a)
+
+print("-"*50)
+print(len(a))
+
+print("-"*50)
+a["kaan"]=53
+print(a)
+
+print("-"*50)
+print(a.keys())
+
+print("-"*50)
+print(a.values())
+
+print("-"*50)
+a.pop("ali")
+print(a)
